f34d492569e5ff5745c6384af8bab6389f76fa72
Filipi Fuchter <filipi87@gmail.com>
2022-03-09 11:10:48 -0300

    Starting to implement setAudioOutputDevice

--- ios/RCTWebRTC/WebRTCModule+DevicesManager.m
+++ ios/RCTWebRTC/WebRTCModule+DevicesManager.m
@@ -13,12 +13,23 @@
 
 @implementation WebRTCModule (DevicesManager)
 
+enum DeviceType {
+    BLUETOOTH,
+    SPEAKER,
+    EARPIECE_HEADSET,
+    BUILT_IN_MICROPHONE,
+    CAMERA_USER,
+    CAMERA_ENVIRONMENT,
+};
+
 RCT_EXPORT_METHOD(enumerateDevices:(RCTResponseSenderBlock)callback)
 {
     NSLog(@"[Daily] enumerateDevice from DevicesManager");
     NSMutableArray *devices = [NSMutableArray array];
     
     [self fillVideoInputDevices:devices];
+    // Whenever any headphones plugged in, it becomes the default audio route even there is also bluetooth device.
+    // And it overwrites the handset(iPhone) option, which means you cannot change to the handset(iPhone).
     [self fillAudioInputDevices:devices];
     [self fillAudioOutputDevices:devices];
     
@@ -72,20 +83,35 @@ RCT_EXPORT_METHOD(enumerateDevices:(RCTResponseSenderBlock)callback)
 
 //TODO implement
 - (void)fillAudioOutputDevices:(NSMutableArray *)devices {
-    // FIXME looks like we will need to create the list of the output devices based on the input devices
-    // https://github.com/sonisuman/AudioPlayer-MultiRoute-Support/tree/master/AudioPlayerMultiRouteSupport/AudioPlayerMultiRouteSupport/AudioPlayerSupport
-    // https://stephen-chen.medium.com/how-to-add-audio-device-action-sheet-to-your-ios-app-e6bc401ccdbc
-    // https://github.com/xialin/AudioSessionManager/blob/master/AudioSessionManager.m
-    NSArray<AVAudioSessionPortDescription *> *availableInputs = [[AVAudioSession sharedInstance] availableInputs];
+    [devices addObject:@{
+                         @"deviceId": [NSNumber numberWithInt:EARPIECE_HEADSET],
+                         @"groupId": @"",
+                         @"label": @"Earpiece\Headset",
+                         @"kind": @"audiooutput",
+                         }];
     
-    NSArray<AVAudioSessionPortDescription *> *outputs = [[[AVAudioSession sharedInstance] currentRoute] outputs];
-    for (AVAudioSessionPortDescription *output in outputs) {
-        NSLog(@"[Daily] Possible outuput %@s %@s", [output portName], [output portType]);
-        if( [output.dataSources count] ){
-            NSLog(@"%@",[NSString stringWithFormat:@"Port has %d data sources",(unsigned)[output.dataSources count] ]);
-            NSLog(@"%@",[NSString stringWithFormat:@"Selected data source:%@",  output.selectedDataSource.dataSourceName]);
-        }
-    }
+    [devices addObject:@{
+                         @"deviceId": [NSNumber numberWithInt:SPEAKER],
+                         @"groupId": @"",
+                         @"label": @"Speaker",
+                         @"kind": @"audiooutput",
+                         }];
+
+//FIXME all we need is to findout if bluetooth is or not connected
+//    // FIXME looks like we will need to create the list of the output devices based on the input devices
+//    // https://github.com/sonisuman/AudioPlayer-MultiRoute-Support/tree/master/AudioPlayerMultiRouteSupport/AudioPlayerMultiRouteSupport/AudioPlayerSupport
+//    // https://stephen-chen.medium.com/how-to-add-audio-device-action-sheet-to-your-ios-app-e6bc401ccdbc
+//    // https://github.com/xialin/AudioSessionManager/blob/master/AudioSessionManager.m
+//    NSArray<AVAudioSessionPortDescription *> *availableInputs = [[AVAudioSession sharedInstance] availableInputs];
+//
+//    NSArray<AVAudioSessionPortDescription *> *outputs = [[[AVAudioSession sharedInstance] currentRoute] outputs];
+//    for (AVAudioSessionPortDescription *output in outputs) {
+//        NSLog(@"[Daily] Possible outuput %@s %@s", [output portName], [output portType]);
+//        if( [output.dataSources count] ){
+//            NSLog(@"%@",[NSString stringWithFormat:@"Port has %d data sources",(unsigned)[output.dataSources count] ]);
+//            NSLog(@"%@",[NSString stringWithFormat:@"Selected data source:%@",  output.selectedDataSource.dataSourceName]);
+//        }
+//    }
 }
 
 - (BOOL)isBluetoothDevice:(NSString*)portType {
@@ -101,8 +127,47 @@ RCT_EXPORT_METHOD(enumerateDevices:(RCTResponseSenderBlock)callback)
 }
 
 
-RCT_EXPORT_METHOD(setAudioOutputDevice:(NSString *)deviceId) {
+RCT_EXPORT_METHOD(setAudioOutputDevice:(nonnull NSNumber*)deviceId) {
     NSLog(@"[Daily] setAudioOutputDevice: %@", deviceId);
+    
+    // Ducking other apps' audio implicitly enables allowing mixing audio with
+    // other apps, which allows this app to stay alive in the backgrounnd during
+    // a call (assuming it has the voip background mode set).
+    AVAudioSessionCategoryOptions categoryOptions = (AVAudioSessionCategoryOptionDuckOthers);
+
+    switch ([deviceId intValue]) {
+        case EARPIECE_HEADSET:
+            //we dont need to add anything more
+            NSLog(@"[Daily] configuring output to EARPIECE_HEADSET");
+            break;
+        case SPEAKER:
+            NSLog(@"[Daily] configuring output to SPEAKER");
+            categoryOptions |= AVAudioSessionCategoryOptionDefaultToSpeaker;
+            break;
+        case BLUETOOTH:
+            NSLog(@"[Daily] configuring output to BLUETOOTH");
+            categoryOptions |= AVAudioSessionCategoryOptionAllowBluetooth;
+            break;
+        default:
+            NSLog(@"[Daily] not recognized output type");
+            break;
+    }
+    
+    [self audioSessionSetCategory:AVAudioSessionCategoryPlayAndRecord toSession:[AVAudioSession sharedInstance] options:categoryOptions];
+}
+
+- (void)audioSessionSetCategory:(NSString *)audioCategory
+                      toSession:(AVAudioSession *)audioSession
+                        options:(AVAudioSessionCategoryOptions)options
+{
+  @try {
+    [audioSession setCategory:audioCategory
+                  withOptions:options
+                        error:nil];
+    NSLog(@"[Daily] audioSession.setCategory: %@, withOptions: %lu success", audioCategory, (unsigned long)options);
+  } @catch (NSException *e) {
+    NSLog(@"[Daily] audioSession.setCategory: %@, withOptions: %lu fail: %@", audioCategory, (unsigned long)options, e.reason);
+  }
 }
 
 @end
