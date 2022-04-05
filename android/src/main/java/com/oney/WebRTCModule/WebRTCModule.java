220a0bb0fc8e5b98d8c074f224682f7e4d876179
Filipi Fuchter <filipi87@gmail.com>
2022-04-05 17:05:54 -0300

    Start to support ondevicechange for IOs

--- android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
+++ android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
@@ -1023,6 +1023,11 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
+    public void startMediaDevicesEventMonitor() {
+        //TODO implementar
+    }
+
+    @ReactMethod
     public void getAudioRoute(Promise promise) {
         ThreadUtils.runOnExecutor(() -> {
             int audioRoute = this.webRTCDevicesManager.getAudioRoute();
