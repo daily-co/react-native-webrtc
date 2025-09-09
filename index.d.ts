6cbb93e39ed8a5cf9c04d8b8bd4c89857bc58a40
Filipi Fuchter <filipi87@gmail.com>
2025-09-09 18:28:26 -0300

    Adding the new types.

--- index.d.ts
+++ index.d.ts
@@ -2,19 +2,21 @@
 
 import mediaDevices from './lib/typescript/MediaDevices';
 import MediaStream from './lib/typescript/MediaStream';
-import MediaStreamTrack from './lib/typescript/MediaStreamTrack';
+import MediaStreamTrack, { type MediaTrackSettings } from './lib/typescript/MediaStreamTrack';
 import permissions from './lib/typescript/Permissions';
+import RTCAudioSession from './lib/typescript/RTCAudioSession';
 import RTCErrorEvent from './lib/typescript/RTCErrorEvent';
 import RTCIceCandidate from './lib/typescript/RTCIceCandidate';
+import RTCPIPView, { startIOSPIP, stopIOSPIP } from './lib/typescript/RTCPIPView';
 import RTCPeerConnection from './lib/typescript/RTCPeerConnection';
 import RTCRtpReceiver from './lib/typescript/RTCRtpReceiver';
 import RTCRtpSender from './lib/typescript/RTCRtpSender';
 import RTCRtpTransceiver from './lib/typescript/RTCRtpTransceiver';
 import RTCSessionDescription from './lib/typescript/RTCSessionDescription';
-import RTCView from './lib/typescript/RTCView';
+import RTCView, { type RTCVideoViewProps, type RTCIOSPIPOptions } from './lib/typescript/RTCView';
 import ScreenCapturePickerView from './lib/typescript/ScreenCapturePickerView';
 import { ViewStyle } from 'react-native';
-export { RTCIceCandidate, RTCPeerConnection, RTCSessionDescription, RTCView, ScreenCapturePickerView, RTCRtpTransceiver, RTCRtpReceiver, RTCRtpSender, RTCErrorEvent, MediaStream, MediaStreamTrack, mediaDevices, permissions, registerGlobals };
+export { RTCIceCandidate, RTCPeerConnection, RTCSessionDescription, RTCView, RTCPIPView, ScreenCapturePickerView, RTCRtpTransceiver, RTCRtpReceiver, RTCRtpSender, RTCErrorEvent, RTCAudioSession, MediaStream, MediaStreamTrack, type MediaTrackSettings, type RTCVideoViewProps, type RTCIOSPIPOptions, mediaDevices, permissions, registerGlobals, startIOSPIP, stopIOSPIP };
 declare function registerGlobals(): void;
 
 // Daily types
